adc820813e99e7d0e9aa9987425ff90e847c27f7
Bisratworku <101055036+Bisratworku@users.noreply.github.com>
2026-07-23 02:54:44 -0700

    optimized git clone command in install.bat to use --depth=1

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
-git clone https://github.com/Bisratworku/ASCII-Art.git
+git clone --depth=1 https://github.com/Bisratworku/ASCII-Art.git
 cd ASCII-Art
 pip install -r requirements.txt
 echo "Installation is Finished"
